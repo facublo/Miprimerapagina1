a9524a39ad776594a581afa266763cd2e73ad28e
facublo <135169057+facublo@users.noreply.github.com>
2023-06-10 19:00:10 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const miBoton = document.getElementById("miBoton");
+function handleClick (){
+alert("hola has hecho click en el boton");
+}
+miBoton.addEventListener("click", handleClick);
---
+++ script.js
@@ -0,0 +1,8 @@
+function showalert ()
+{
+alert("hola,esta es una aleta de javascript")
+}
+function suma(a,b){
+	return: a+b;
+}
+console.log(suma(3,7));
